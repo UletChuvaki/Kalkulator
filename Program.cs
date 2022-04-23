f5a3d04e2935f10ada822be0852da62b8af65525
UletChuvaki <moisei.98@inbox.ru>
2022-04-23 09:30:24 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kalkulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double a;
+            double b;
+            
+
+            Console.WriteLine("Введите число1");
+            a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите символ");
+            string i = Console.ReadLine();
+            switch (i)
+            {
+                case "+":
+                    break;
+
+                case "-":
+                    break;
+
+                case "*":
+                    break;
+
+
+                case "/":
+                    break;
+
+                default:
+                    Console.WriteLine("Вы ввели неизвестный символ");
+                    break;
+
+            }
+
+            Console.WriteLine("Введите число");
+            b = double.Parse(Console.ReadLine());
+
+            double res4 = a + b;
+            double res1 = a - b;
+            double res2 = a * b;
+            double res3 = a / b;
+
+            Console.WriteLine("Ответ" + res4 , res1 , res2, res3 ) ;
+
+
+            
+             
+
+
+        }
+    }
+}
